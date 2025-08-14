73c5c88a450a5afa063eab16ecc1b89e5b935050
Farhod <farhod@example.com>
2025-08-14 11:25:11 +0500

    time edit2

--- chat/utils.py
+++ chat/utils.py
@@ -1,8 +1,17 @@
+import datetime
+
 import pytz
 from django.utils import timezone
 
-def to_user_timezone(dt, tz_name="Asia/Tashkent"):
-    if not dt:
-        return None
-    user_tz = pytz.timezone(tz_name)
-    return timezone.localtime(dt, user_tz).isoformat()
+
+def to_user_timezone(dt, tz="Asia/Tashkent"):
+    if isinstance(dt, str):
+        # ISO format string bo‘lsa — datetime’ga aylantiramiz
+        try:
+            dt = datetime.fromisoformat(dt)
+        except ValueError:
+            return dt  # noto‘g‘ri format bo‘lsa, o‘zini qaytaramiz
+
+    if timezone.is_naive(dt):
+        dt = timezone.make_aware(dt, timezone.utc)
+    return dt.astimezone(pytz.timezone(tz))
